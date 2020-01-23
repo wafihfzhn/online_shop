c2b28dc086a5e23cacb7d0dfc9bfddacbfb98977
wafihfzhn <hafizhuddin96@gmail.com>
2020-01-24 01:20:01 +0800

    Add feature for add products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :description, :price)
+  end
 end
