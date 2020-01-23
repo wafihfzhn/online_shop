97fd6b6e060daf6c253cbb3a7ffb052fcf23fe98
wafihfzhn <hafizhuddin96@gmail.com>
2020-01-24 02:24:58 +0800

    Listing products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_224505) do
+ActiveRecord::Schema.define(version: 2020_01_23_181221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_224505) do
     t.string "image_data"
     t.string "name"
     t.string "description"
-    t.string "price"
+    t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
